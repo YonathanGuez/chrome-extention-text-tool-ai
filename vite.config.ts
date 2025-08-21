a2dd532e2bf1889124815873627a1e64cb9cbe2c
yonathanguez <yonath-g@hotmail.fr>
2025-08-21 17:49:44 +0200

    add pipeline project

--- vite.config.ts
+++ vite.config.ts
@@ -15,13 +15,8 @@ export default defineConfig({
     }),
   ],
   build: {
-    // The 'assetsDir' option specifies the directory for generated assets
-    // (like JS, CSS, and images) relative to the 'outDir'.
     // By setting it to an empty string (''), we tell Vite to place the
     // assets directly inside the output directory, effectively flattening the structure.
     assetsDir: "",
-
-    // You can also change the name of the output directory if needed.
-    // outDir: 'build' // for example, to build to a 'build' folder instead of 'dist'
   },
 });
